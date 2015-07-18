88bfe08d35ed72d6e09f3b8d0792f37b39fe5a57
Esben Baden Smith <esbenbadensmith@Esbens-MacBook-Pro.local>
2015-07-18 11:13:23 -0700

    added hover event to landing page

---
+++ public/scripts/script.js
@@ -0,0 +1,24 @@
+
+$("#techno").on({
+    mouseenter: function () {
+        //stuff to do on mouse enter
+        $(this).css({"background-color": "orange"});
+    },
+    mouseleave: function () {
+        //stuff to do on mouse leave
+        $(this).css({"background-color": "black"});
+    }
+    
+});
+
+
+$("#house").on({
+    mouseenter: function () {
+        //stuff to do on mouse enter
+        $(this).css({"background-color": "orange"});
+    },
+    mouseleave: function () {
+        //stuff to do on mouse leave
+        $(this).css({"background-color": "#00CCFF"});
+    }
+});
