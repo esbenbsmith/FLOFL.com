c8289b387412bf3d9496b30efd36d70a1b2747ff
Esben Smith <esbenbsmith@gmail.com>
2015-07-18 18:06:37 -0700

    added server, landing and house updated

---
+++ README.md
@@ -0,0 +1,5 @@
+FLOFL.com 
+
+Web Application that is an Automated DJ for House/Techno
+
+Esben Baden Smith
---
+++ server.js
@@ -0,0 +1,33 @@
+var express = require('express'),
+    app = express(),
+    bodyParser = require('body-parser'),
+    _ = require("underscore");
+
+
+
+// configure bodyParser (for handling data)
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+
+// serve js and css files from public folder
+app.use(express.static(__dirname + '/public'));
+
+// root route (serves index.html)
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/public/views/landing.html');
+});
+
+app.get('/house', function (req, res) {
+  res.sendFile(__dirname + '/public/views/indexhouse.html');
+});
+
+app.get('/techno', function (req, res) {
+  res.sendFile(__dirname + '/public/views/indextechno.html');
+});
+
+
+// set server to localhost:3000
+app.listen(3000, function () {
+  console.log('server started on localhost:3000');
+});
