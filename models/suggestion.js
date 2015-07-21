e3d5c280ac40ae947a974572b3aef66c40659603
Esben Smith <esbenbsmith@gmail.com>
2015-07-21 11:00:03 -0700

    fifth time

--- models/suggestion.js
+++ models/suggestion.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 var SuggestionSchema = new Schema({
-  text:{ type:String, required: true}
+  text: { type: String, required: true}
 })
 
 var Suggestion = mongoose.model('Suggestion', SuggestionSchema);
