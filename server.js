d4fdb081c6266b65bc9d9704965cb155c70a00ed
Esben Baden Smith <esbenbadensmith@Esbens-MacBook-Pro.local>
2015-07-22 16:57:20 -0700

    End of day

--- server.js
+++ server.js
@@ -24,9 +24,41 @@ app.use(cors());
 // serve js and css files from public folder
 app.use(express.static(__dirname + "/public"));
 
+app.use(session({
+  saveUninitialized: true,
+  resave: true,
+  secret: 'SuperSecretCookie',
+  cookie: { maxAge: 60000 }
+}));
+
+
+// middleware to manage sessions
+app.use('/login', function (req, res, next) {
+  // saves userId in session for logged-in user
+  req.login = function (user) {
+    req.session.userId = user.id;
+  };
+
+  // finds user currently logged in based on `session.userId`
+  req.currentUser = function (callback) {
+    User.findOne({_id: req.session.userId}, function (err, user) {
+      req.user = user;
+      callback(null, user);
+    });
+  };
+
+  // destroy `session.userId` to log out user
+  req.logout = function () {
+    req.session.userId = null;
+    req.user = null;
+  };
+
+  next();
+});
 
 //API ROUTES:
-//SUGGESTIONS QUERY
+
+// API SUGGESTIONS QUERY
 app.get("/api/suggestion", function (req, res) {
   console.log(Suggestion);
   Suggestion.find().sort("-_id").exec(function(err, suggestions){
@@ -35,7 +67,7 @@ app.get("/api/suggestion", function (req, res) {
   });
 });
 
-//SUGGESTIONS CREATE
+// API SUGGESTIONS CREATE
 app.post("/api/suggestion", function (req, res){
   //SAVE TO DB
   var suggestion = new Suggestion({
@@ -47,31 +79,62 @@ app.post("/api/suggestion", function (req, res){
   });
 });
 
-//USER CREATE
+
+// AUTH ROUTES (SIGN UP, LOG IN, LOG OUT)
+
 // create new user with secure password
+app.post('/users', function (req, res) {
+  var newUser = req.body.user;
+  User.createSecure(newUser, function (err, user) {
+    // log in user immediately when created
+    // req.login(user);
+    console.log(newUser);
+    res.redirect('/');
+  });
+});
+
+// authenticate user and set session
+app.post('/login', function (req, res) {
+  var userData = req.body.user;
+  User.authenticate(userData.email, userData.password, function (err, user) {
+    req.login(user);
+    res.redirect('/');
+  });
+});
+
+// log out user (destroy session)
+app.get('/logout', function (req, res) {
+  req.logout();
+  res.redirect('/');
+});
 
 
-//PUBLIC ROUTES
+//STATIC ROUTES
 
 // root route (serves index.html)
 app.get('/', function (req, res) {
   res.sendFile(__dirname + "/public/views/landing.html");
+  
 });
 
+//house
 app.get('/house', function (req, res) {
   res.sendFile(__dirname + "/public/views/indexhouse.html");
 });
 
+//techno
 app.get('/techno', function (req, res) {
   res.sendFile(__dirname + "/public/views/indextechno.html");
 });
 
+//about
 app.get('/about', function (req, res) {
   res.sendFile(__dirname + "/public/views/about.html");
 });
 
-app.get('/login', function (req, res) {
-  res.sendFile(__dirname + "/public/views/login.html");
+//login/signup
+app.get('/registration', function (req, res) {
+  res.sendFile(__dirname + "/public/views/registration.html");
 });
 
 
